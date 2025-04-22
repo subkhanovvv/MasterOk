18793610400a146a1c565ac50ed7ae8960567757
OTABEK <subkhanovvv@gmail.com>
2025-04-22 23:24:05 +0500

    Update admin sidebar links for consistency and correct routing

--- resources/views/layouts/sidebar/admin-sidebar.blade.php
+++ resources/views/layouts/sidebar/admin-sidebar.blade.php
@@ -1,21 +1,21 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link" href="../index.html">
+            <a class="nav-link" href="{{ route('index') }}">
                 <i class="mdi mdi-grid-large menu-icon"></i>
                 <span class="menu-title">Dashboard</span>
             </a>
         </li>
-        <li class="nav-item nav-category">Products</li>
+        <li class="nav-item nav-category">Product</li>
         <li class="nav-item">
-            <a class="nav-link" href="{{route('category')}}" >
+            <a class="nav-link" href="{{ route('category') }}">
                 <i class="menu-icon mdi mdi-floor-plan"></i>
                 <span class="menu-title">Categories</span>
                 <i class="menu-arrow"></i>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link"  href="{{route('product')}}" >
+            <a class="nav-link" href="{{ route('product') }}">
                 <i class="menu-icon mdi mdi-arrange-send-backward"></i>
                 <span class="menu-title">Products</span>
                 <i class="menu-arrow"></i>
@@ -25,6 +25,29 @@
             <a class="nav-link" href="">
                 <i class="menu-icon mdi mdi-barcode"></i>
                 <span class="menu-title">Barcodes</span>
+                <i class="menu-arrow"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="menu-icon mdi mdi-warehouse"></i>
+                <span class="menu-title">SKlad</span>
+                <i class="menu-arrow"></i>
+            </a>
+        </li>
+        <li class="nav-item nav-category">Brand</li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('brand') }}">
+                <i class="menu-icon mdi mdi-tag"></i>
+                <span class="menu-title">Brands</span>
+                <i class="menu-arrow"></i>
+            </a>
+        </li>
+        <li class="nav-item nav-category">History</li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('brand') }}">
+                <i class="menu-icon mdi mdi-history"></i>
+                <span class="menu-title">Brands</span>
                 <i class="menu-arrow"></i>
             </a>
         </li>
