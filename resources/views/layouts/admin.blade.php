b9194674da57e72029152f2b9c13055343e49227
OTABEK <subkhanovvv@gmail.com>
2025-05-07 20:40:04 +0500

    Refactor layout files; update admin footer content for consistency, enhance alert script formatting, and improve overall structure in admin blade files.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,11 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>MasterOk</title>
-    <!-- plugins:css -->
+    <title>MasterOk | SINV</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/vendors/feather/feather.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
@@ -14,16 +12,9 @@
     <link rel="stylesheet" href="{{ asset('admin/assets/vendors/typicons/typicons.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/vendors/simple-line-icons/css/simple-line-icons.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/vendors/css/vendor.bundle.base.css') }}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
     <link rel="stylesheet" href="{{ asset('admin/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('admin/assets/js/select.dataTables.min.css') }}">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
     <link rel="stylesheet" href="{{ asset('admin/assets/css/vertical-layout-light/style.css') }}">
-    <!-- endinject -->
     <link rel="shortcut icon" href="{{ asset('admin/assets/images/favicon.ico') }}" />
 </head>
 
--- resources/views/layouts/footer/admin-footer.blade.php
+++ resources/views/layouts/footer/admin-footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Otabek Subkhanov | MasterOk</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © <span id="year"></span>
+        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">SINV--v1.0</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">© <span id="year"></span> - MasterOK
     </div>
 </footer>
 <script>
