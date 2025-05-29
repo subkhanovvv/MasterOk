d270d65133657a860d083ad1c9e4e1c08fd1f727
OTABEK <subkhanovvv@gmail.com>
2025-05-29 07:15:35 +0500

    feat: Initialize SettingController with basic structure and necessary imports

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+
+class SettingController extends Controller
+{
+  
+}
