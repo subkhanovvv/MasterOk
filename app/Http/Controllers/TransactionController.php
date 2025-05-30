bac10bb6d504a124e6ac81f142fd037201aeba0e
OTABEK <subkhanovvv@gmail.com>
2025-05-30 12:23:24 +0500

    refactor: Remove unused model imports in TransactionController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,18 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
-use App\Models\Category;
-use App\Models\Product;
 use App\Models\ProductActivity;
-use App\Models\ProductActivityItems;
-use App\Models\Supplier;
 use Carbon\Carbon;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Validator as FacadesValidator;
-use Illuminate\Validation\Validator;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\ReportExport;
 use Barryvdh\DomPDF\Facade\Pdf as FacadePdf;
